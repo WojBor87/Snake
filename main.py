d8912299e7056144308c3b56f96d9f6385230f1f
Wojciech Borawski <w.borawski@ihar.edu.pl>
2021-11-24 13:50:57 +0100

    Collisions with tail added

--- main.py
+++ main.py
@@ -33,11 +33,20 @@ while game_is_on:
     # Detect collisions with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.score_write()
 
     # Detect collisions with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    # Detect collisions with tail
+    for segment in snake.snake:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
